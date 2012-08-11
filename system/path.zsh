45fea25aa50b0e19a8a52dab4ae022c0657da716
Leroux Cifer <leroux@fezrev.com>
2012-08-11 02:53:21 -0500

    PATH change.

--- system/path.zsh
+++ system/path.zsh
@@ -1,3 +1,13 @@
 export PATH="./bin:$HOME/.rbenv/shims:/usr/local/bin:/usr/local/sbin:$HOME/.sfs:$ZSH/bin:$PATH"
 
+# Macports
+export PATH=$PATH:/opt/local/bin:/opt/local/sbin
+
+# Go
+export GOROOT=/usr/local/go
+export PATH=$PATH:$GOROOT/bin
+
+# LuaRocks
+export PATH=$PATH:/opt/local/share/luarocks/bin
+
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
