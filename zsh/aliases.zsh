02a6bfe87c39c1797ec35fbcf9d3a9f42d15a923
Leroux Cifer <leroux@fezrev.com>
2012-09-15 19:08:53 -0500

    Update.

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,3 +1,3 @@
 alias reload!='. ~/.zshrc'
 alias jelli='mplayer http://jelli.ic.llnwd.net/stream/jelli_stream_31'
-alias tc='/Applications/TrueCrypt.app/Contents/MacOS/TrueCrypt /Users/leroux/Dropbox/leroux.tc /Users/leroux/leroux.tc.mnt'
+alias tc='/Applications/TrueCrypt.app/Contents/MacOS/TrueCrypt /Users/leroux/Dropbox/leroux.tc'
